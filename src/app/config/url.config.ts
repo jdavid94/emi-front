f9b7b88a84009ecbd567f1a7190d4e615d4889de
jdavi <jdavid94@hotmail.com>
2024-11-06 15:38:20 +0100

    config implemented

---
+++ src/app/config/url.config.ts
@@ -0,0 +1,3 @@
+export const UrlConfig = {
+  calculateEmiUrl: 'http://localhost:8080/api/calculate-emi',
+};
--- src/app/services/loan.service.ts
+++ src/app/services/loan.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoanRequest } from '../models/loan-request.model';
 import { Observable } from 'rxjs';
+import { UrlConfig } from '../config/url.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoanService {
 
-  private apiUrl = 'http://localhost:8081/api/calculate-emi';
+  private apiUrl = UrlConfig.calculateEmiUrl;
 
   constructor(private http: HttpClient) {}
 
